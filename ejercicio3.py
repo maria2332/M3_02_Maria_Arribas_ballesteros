a6547de39f7680da08f55915f3d8d0f1999913e8
maria2332 <maria.ballesteros2004@gmail.com>
2022-11-17 18:31:56 +0100

    cambio

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,3 +5,18 @@ Nota: Se tienen que evaluar excepciones concretas, no hacer referencia a Excepti
 paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
 paises["alemania"]
 """
+
+from ast import main 
+
+def error_clave_fuera_diccionario(diccionario, clave):
+    try:
+        clave = diccionario[clave]
+        return clave 
+    except KeyError:
+        print ("La clave {} no se encuentra en la lista y por tanto no se puede devolver su valor correspondiente".format(clave))     
+    return 
+
+error_clave_fuera_diccionario({ "españa":"español", "eeuu":"inglés", "italia":"italiano" } , "alemania")
+
+if __name__=="__main__":
+    main()
