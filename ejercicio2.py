1a0682bde9b4531c45b6eb1c292d40921aaba50d
maria2332 <maria.ballesteros2004@gmail.com>
2022-11-17 18:41:34 +0100

    cambio

--- ejercicio2.py
+++ ejercicio2.py
@@ -13,7 +13,7 @@ def error_elemento_fuera_lista(lista, elemento_lista):
         elemento = lista[elemento_lista]
         return elemento 
     except IndexError:
-        print ("El elemento {} no se encuentra en la lista, ya que la lista solo contiene {} elemento".format(elemento_lista, len(lista)))     
+        print ("El elemento {} no se encuentra en la lista, ya que la lista solo contiene {} elementos".format(elemento_lista, len(lista)))     
     return 
 
 error_elemento_fuera_lista([4, 7, 30, 23, 5], 10)
