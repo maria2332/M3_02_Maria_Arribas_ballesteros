bf718b29f7766ebf2dcb27bd794ecfe1ea6f106e
maria2332 <maria.ballesteros2004@gmail.com>
2022-11-17 18:26:27 +0100

    cambio

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+"""
+Identificar los errores en los siguiente bloques de código y evaluarlos con excepciones especificas para evitar errores no controlados en nuestros programas. Añadem mensajes explicativos para el usuario.
+Nota: Se tienen que evaluar excepciones concretas, no hacer referencia a Exception sin más.
+3) Código a evaluar:
+paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
+paises["alemania"]
+"""
---
+++ ejercicio4.py
@@ -0,0 +1,6 @@
+"""
+Identificar los errores en los siguiente bloques de código y evaluarlos con excepciones especificas para evitar errores no controlados en nuestros programas. Añadem mensajes explicativos para el usuario.
+Nota: Se tienen que evaluar excepciones concretas, no hacer referencia a Exception sin más.
+4) Localiza el error en el siguiente bloque de código. Crea una excepción para evitar que el programa se bloquee y además explica en un mensaje al usuario la causa y/o solución:
+resultado = "2" + 10
+"""
